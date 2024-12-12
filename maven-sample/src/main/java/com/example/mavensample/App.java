9cdc0cfcc9e0ab9b9654ac83ef266b0b0ed92c08
Abhinav <abhinavgunty2005@gmail.com>
2024-12-13 00:06:33 +0530

    ....

---
+++ maven-sample/src/main/java/com/example/mavensample/App.java
@@ -0,0 +1,13 @@
+package com.example.mavensample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
